fd850a6eb1333d3f1a3da1bd7d577175b16b0159
salwaahmadz <salwa.ahmad@lumoshive.com>
2024-06-28 23:41:57 +0700

    update tampilan

--- app.py
+++ app.py
@@ -48,9 +48,9 @@ def get_keyword_frequencies(texts, keywords):
 def adjust_prediction_for_positive_words(prediction, text, positive_keywords):
     words = preprocess_text(text)
     positive_count = sum(1 for word in words if word in positive_keywords)
-    # Adjust the prediction based on the number of positive words found
+    # MENYESUKAN PREDIKSI UNTUK KATA KUNCI POSITIF
     if positive_count > 0:
-        adjustment = positive_count * 0.02  # Example adjustment factor
+        adjustment = positive_count * 0.02
         prediction = max(0, prediction - adjustment)
     return prediction
 
@@ -101,7 +101,7 @@ model, tokenizer = load_model_and_tokenizer()
 data, data2 = load_data()
 # model.summary()
 
-# MAIN CODE
+# DATASET
 st.header("APLIKASI IDENTIFIKASI ANOMALI KESEHATAN MENTAL PADA KOMENTAR")
 st.markdown("**Developed By Salwa Ahmad Zanjabila | Informatika Universitas Pembangunan Jaya 2020**")
 st.write("")
@@ -110,6 +110,7 @@ comments = [comment['Preprocess'] for comment in data['data']]
 dataset = pd.DataFrame({'Komentar': comments})
 st.write(dataset)
 
+# KATA KUNCI
 st.subheader("Kata Kunci Yang Digunakan Pada Aplikasi")
 col1, col2 = st.columns(2)
 keyword_list_df_negatif = pd.DataFrame(mental_health_keywords, columns=['Kata Kunci'])
@@ -122,12 +123,45 @@ with col2:
     st.write("Kata Kunci Positif")
     st.write(keyword_list_df_positif)
 
-st.subheader("Data Test Komentar Dari YouTube")
+# FORM INPUT PENGGUNA
+st.subheader("Masukan Komentar Yang Ingin Diidentifikasi (Menggunakan Bahasa Inggris)")
+with st.form(key='form_predict'):
+    text_input = st.text_input(label='Masukkan Komentar')
+    submit_button = st.form_submit_button(label='Submit')
+
+    if submit_button:
+        with st.spinner("Predicting..."):
+            input_sequence = tokenizer.texts_to_sequences([text_input])
+            input_padded = pad_sequences(input_sequence, maxlen=537)
+
+            input_prediction = model.predict(input_padded)[0][0]
+            
+            input_prediction = adjust_prediction_for_positive_words(input_prediction, text_input, positive_keywords)
+            
+            input_label = label_prediction(input_prediction)
+            input_prediction_percent = input_prediction * 100
+
+            st.write(f"Komentar: {text_input}")
+            st.write(f"Hasil Prediksi: {input_prediction_percent:.2f}%")
+            st.write(f"Label: {input_label}")
+
+            col1, col2 = st.columns(2)
+            with col1:
+                input_keywords = get_keyword_frequencies([text_input], mental_health_keywords)
+                st.write("Kata Kunci negatif dalam Komentar yang Dimasukkan:")
+                st.write(pd.DataFrame(input_keywords, columns=['Kata Kunci', 'Frekuensi']))
+            with col2:
+                input_keywords = get_keyword_frequencies([text_input], positive_keywords)
+                st.write("Kata Kunci positif dalam Komentar yang Dimasukkan:")
+                st.write(pd.DataFrame(input_keywords, columns=['Kata Kunci', 'Frekuensi']))
+
+# DATA TEST
+st.subheader("Contoh Dengan Data Test Komentar Dari YouTube")
 comments2 = [comment2['Comments'] for comment2 in data2['data']]
 datatest = pd.DataFrame({'Komentar': comments2})
 st.write(datatest)
 
-st.subheader("Kata Kunci Yang Paling Sering Muncul")
+st.subheader("Kata Kunci Yang Paling Sering Muncul Pada Data Test")
 negatif_keywords_frequencies = get_keyword_frequencies(datatest['Komentar'], mental_health_keywords)
 negatif_keywords_frequencies_df = pd.DataFrame(negatif_keywords_frequencies, columns=['Kata Kunci', 'Frekuensi'])
 
@@ -141,21 +175,20 @@ with col2:
     st.write(positif_keywords_frequencies_df)
 
 if 'predictions' not in st.session_state:
-    sequence = tokenizer.texts_to_sequences(datatest['Komentar'])
-    padded = pad_sequences(sequence, maxlen=537)
-    predictions = model.predict(padded)
-    st.session_state['predictions'] = predictions
+    with st.spinner("Identifikasi dan Prediksi Data Test Komentar..."):
+        sequence = tokenizer.texts_to_sequences(datatest['Komentar'])
+        padded = pad_sequences(sequence, maxlen=537)
+        predictions = model.predict(padded)
+        st.session_state['predictions'] = predictions
 else:
     predictions = st.session_state['predictions']
 
 df_predictions = pd.DataFrame({'Komentar': comments2, 'Label': predictions.flatten(), 'Prediksi': predictions.flatten()})
 
-# Adjust predictions based on positive keywords
 df_predictions['Prediksi'] = df_predictions.apply(
     lambda row: adjust_prediction_for_positive_words(row['Prediksi'], row['Komentar'], positive_keywords), axis=1
 )
 
-# PREDIKSI KOMENTAR
 df_predictions['Label'] = df_predictions['Prediksi'].apply(label_prediction)
 df_predictions['Prediksi (%)'] = df_predictions['Prediksi'] * 100
 st.subheader("Hasil Identifikasi Data Test Komentar")
@@ -165,39 +198,3 @@ st.subheader("Chart Hasil Identifikasi Berdasarkan Label")
 label_counts = df_predictions.groupby('Label').size().reset_index(name='Jumlah')
 st.bar_chart(label_counts.set_index('Label'))
 st.write(label_counts)
-
-st.subheader("Masukan Komentar Yang Ingin Di Identifikasi (Menggunakan Bahasa Inggris)")
-with st.form(key='form_predict'):
-    text_input = st.text_input(label='Masukkan Komentar')
-    submit_button = st.form_submit_button(label='Submit')
-
-    if submit_button:
-        # Preprocess the input text
-        input_sequence = tokenizer.texts_to_sequences([text_input])
-        input_padded = pad_sequences(input_sequence, maxlen=537)
-
-        # Predict the input text
-        input_prediction = model.predict(input_padded)[0][0]
-        
-        # Adjust prediction for positive words
-        st.write(f"Komentar: {text_input}")
-        # st.write(f"Prediksi Awal: {input_prediction:.2f}%")
-        input_prediction = adjust_prediction_for_positive_words(input_prediction, text_input, positive_keywords)
-        
-        input_label = label_prediction(input_prediction)
-        input_prediction_percent = input_prediction * 100
-
-        # Display the result
-        st.write(f"Hasil Prediksi: {input_prediction_percent:.2f}%")
-        st.write(f"Label: {input_label}")
-
-        # Display keyword frequencies in the input text
-        col1, col2 = st.columns(2)
-        with col1:
-            input_keywords = get_keyword_frequencies([text_input], mental_health_keywords)
-            st.write("Kata Kunci negatif dalam Komentar yang Dimasukkan:")
-            st.write(pd.DataFrame(input_keywords, columns=['Kata Kunci', 'Frekuensi']))
-        with col2:
-            input_keywords = get_keyword_frequencies([text_input], positive_keywords)
-            st.write("Kata Kunci positif dalam Komentar yang Dimasukkan:")
-            st.write(pd.DataFrame(input_keywords, columns=['Kata Kunci', 'Frekuensi']))
